f683ec38e92a3b4cd99535b3ccdc48110d1f4e7f
Blockchain-Key <1416893908@qq.com>
2022-11-10 17:53:15 +0800

    modified umirc.ts

--- frontend/.umirc.ts
+++ frontend/.umirc.ts
@@ -2,4 +2,6 @@ export default {
   npmClient: "yarn",
   tailwindcss: {},
   plugins: ["@umijs/plugins/dist/tailwindcss"],
+  base: process.env.NODE_ENV === 'production' ? './' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
 };
