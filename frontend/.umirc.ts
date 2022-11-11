3b6173ae358a6f09d87d9cbcfd93e311f482a729
Blockchain-Key <1416893908@qq.com>
2022-11-11 15:55:43 +0800

    add links and title

--- frontend/.umirc.ts
+++ frontend/.umirc.ts
@@ -2,4 +2,8 @@ export default {
   npmClient: "yarn",
   tailwindcss: {},
   plugins: ["@umijs/plugins/dist/tailwindcss"],
+  links: [
+    { rel: "icon", href: "/logo.png", type: "image/x-icon" },
+  ],
+  title:"MoveDID",
 };
